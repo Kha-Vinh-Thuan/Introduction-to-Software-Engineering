40b2c0ca3052b1ab8cb18d7d158ce16cb88f136a
trungle <lttrung2418@clc.fitus.edu.vn>
2026-07-15 21:01:49 +0700

    refector(backend) Chuyển DB từ SQLite3 sang PostgresSQL và thêm Products DB (Northwind)

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,15 +1,14 @@
-import sqlite3
+import psycopg
+from psycopg.rows import dict_row
 from app.core.config import settings
 
 
-def getConnection() -> sqlite3.Connection:
-    conn = sqlite3.connect(settings.database_path)
-    conn.row_factory = sqlite3.Row
+def getConnection() -> psycopg.Connection:
+    conn = psycopg.connect(settings.database_path, row_factory= dict_row, autocommit=False)
     return conn
 
 
-def getReadOnlyConnection() -> sqlite3.Connection:
-    uri = f"file:{settings.database_path}?mode=ro"
-    conn = sqlite3.connect(uri, uri=True)
-    conn.row_factory = sqlite3.Row
+def getReadOnlyConnection() -> psycopg.Connection:
+    conn = psycopg.connect(settings.database_path, row_factory= dict_row, autocommit=False)
+    conn.read_only = True
     return conn
